4920142b4b7acf6551a78acc89ce42fef75e8590
mira-kine <mirak0419@gmail.com>
2022-06-29 14:24:21 -0700

    working in App before refactoring

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import Auth from './components/Auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBw-s1xaCUYJvB7uX5pyxiRXOwiLRadY08',
@@ -16,12 +16,14 @@ const firebaseConfig = {
 
 firebase.initializeApp({ firebaseConfig });
 
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
-      <header className="App-header">
-        <Auth />
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import { useAuthState } from 'react-firebase-hooks/auth';
+// import React from 'react';
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+// import 'firebase/auth';
+// import { useAuthState } from 'react-firebase-hooks/auth';
 
-const auth = firebase.auth();
-const firestore = firebase.firestore();
+// const auth = firebase.auth();
+// const firestore = firebase.firestore();
 
-export default function Auth() {
-  const [user] = useAuthState(auth);
+// export default function Auth() {
+//   const [user] = useAuthState(auth);
 
-  return (
-    <div>
-      <section>{user ? <ChatRoom /> : <SignIn />}</section>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <section>{user ? <ChatRoom /> : <SignIn />}</section>
+//     </div>
+//   );
+// }
