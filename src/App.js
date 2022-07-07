7c87299dd7cb53d7da246e37d0d3a02396015a97
mira-kine <mirak0419@gmail.com>
2022-07-07 15:35:09 -0700

    providing user context through auth context

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { AuthProvider } from './components/context/AuthContext';
 import SignIn from './components/SignIn/SignIn';
+import ChatRoom from './components/ChatRoom/ChatRoom';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route path="/">
-            <SignIn />
-          </Route>
-        </Switch>
+        <AuthProvider>
+          <Switch>
+            <Route path="/chatroom">
+              <ChatRoom />
+            </Route>
+            <Route path="/">
+              <SignIn />
+            </Route>
+          </Switch>
+        </AuthProvider>
       </Router>
     </div>
   );
