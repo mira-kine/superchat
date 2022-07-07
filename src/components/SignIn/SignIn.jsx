4512a173dfda24e0835b8f88fbf716a8086b1773
mira-kine <mirak0419@gmail.com>
2022-07-07 13:51:37 -0700

    auth providers working, building out chat next

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,29 +1,46 @@
 import React from 'react';
-import 'firebase/app';
-// import { auth } from '../firebase';
-import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
+import '../../firebase';
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithRedirect,
+  FacebookAuthProvider,
+} from 'firebase/auth';
 
 export default function SignIn() {
+  const GProvider = new GoogleAuthProvider();
+  const FbProvider = new FacebookAuthProvider();
+  const auth = getAuth();
+
+  // const logIn = signInWithPopup(auth, provider)
+  //   .then((result) => {
+  //     const credential = GoogleAuthProvider.credentialFromResult(result);
+  //     const token = credential.accessToken;
+  //     const user = result.user;
+  //   })
+  //   .catch((error) => {
+  //     const errorCode = error.code;
+  //     const errorMessage = error.message;
+  //     const email = error.customData.email;
+  //     const credential = GoogleAuthProvider.credentialFromError(error);
+  //   });
+
   return (
     <div>
       <div>
         <h2>Welcome to superchat</h2>
         <div className="login-button google">
-          <button onClick={() => signInWithRedirect(new GoogleAuthProvider())}>
+          <button onClick={() => signInWithRedirect(auth, GProvider)}>
             Sign In with Google
           </button>
         </div>
       </div>
       <br /> <br />
-      {/* <div className="login-button facebook">
-        <button
-          onClick={() =>
-            auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())
-          }
-        >
+      <div className="login-button facebook">
+        <button onClick={() => signInWithRedirect(auth, FbProvider)}>
           Sign In with Facebook
         </button>
-      </div> */}
+      </div>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,19 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+// import firebase from 'firebase/app';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
-export const auth = firebase
-  .initializeApp({
-    apiKey: 'AIzaSyBw-s1xaCUYJvB7uX5pyxiRXOwiLRadY08',
-    authDomain: 'superchat-mkine.firebaseapp.com',
-    projectId: 'superchat-mkine',
-    storageBucket: 'superchat-mkine.appspot.com',
-    messagingSenderId: '761241189116',
-    appId: '1:761241189116:web:b33035f18237017e777579',
-  })
-  .auth();
+// Add in Firebase config
+const firebaseConfig = {
+  apiKey: 'AIzaSyBw-s1xaCUYJvB7uX5pyxiRXOwiLRadY08',
+  authDomain: 'superchat-mkine.firebaseapp.com',
+  projectId: 'superchat-mkine',
+  storageBucket: 'superchat-mkine.appspot.com',
+  messagingSenderId: '761241189116',
+  appId: '1:761241189116:web:b33035f18237017e777579',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase quthentication
+export const auth = getAuth(app);
