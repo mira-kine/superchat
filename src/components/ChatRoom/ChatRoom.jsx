1a8e6f9b0ec67313c06ab2c0edd5720800d60c12
mira-kine <mirak0419@gmail.com>
2022-07-18 23:10:13 -0700

    able to add group chat rooms,select upon click

--- src/components/ChatRoom/ChatRoom.jsx
+++ src/components/ChatRoom/ChatRoom.jsx
@@ -1,5 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { useConversations } from '../context/ConversationsProvider';
 
 export default function ChatRoom() {
-  return <div>Hello Chat Room</div>;
+  const { conversations, selectConversationIndex } = useConversations();
+  return (
+    <ListGroup variant="flush">
+      {conversations.map((conversation, index) => (
+        <ListGroup.Item
+          key={index}
+          action
+          onClick={() => selectConversationIndex(index)}
+          active={conversation.selected}
+        >
+          {conversation.recipients
+            .map((recipient) => recipient.name)
+            .join(', ')}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
 }
