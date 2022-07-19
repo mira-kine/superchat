2a1f4822234f9aa822529e4182e8e29af1b72dbb
mira-kine <mirak0419@gmail.com>
2022-07-18 22:59:10 -0700

    created conversationsProvider and formatted conversation in it to have ease of setting it to state and rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import { AuthProvider } from './components/context/AuthContext';
 import SignIn from './components/SignIn/SignIn';
@@ -6,6 +6,7 @@ import ChatRoom from './components/ChatRoom/ChatRoom';
 import useLocalStorage from './hooks/useLocalStorage';
 import UserPage from './components/UserPage/UserPage';
 import { ContactsProvider } from './components/context/ContactsProvider';
+import { ConversationsProvider } from './components/context/ConversationsProvider';
 
 function App() {
   // useLocalStorage hook to persist the user's id
@@ -15,13 +16,15 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/chatroom">
-            <ChatRoom />
-          </Route>
           <ContactsProvider>
-            <Route path="/">
-              {id ? <UserPage id={id} /> : <SignIn onIdSubmit={setId} />}
-            </Route>
+            <ConversationsProvider>
+              <Route path="/chatroom">
+                <ChatRoom />
+              </Route>
+              <Route path="/">
+                {id ? <UserPage id={id} /> : <SignIn onIdSubmit={setId} />}
+              </Route>
+            </ConversationsProvider>
           </ContactsProvider>
         </Switch>
       </Router>
--- src/components/NewConversationModal.jsx
+++ src/components/NewConversationModal.jsx
@@ -1,5 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form, Modal } from 'react-bootstrap';
+import { useContacts } from './context/ContactsProvider';
+import { useConversations } from './context/ConversationsProvider';
 
-export default function NewConversationModal() {
-  return <div>NewConversationModal</div>;
+export default function NewConversationModal({ closeModal }) {
+  const [selectedContactIds, setSelectedContactIds] = useState([]);
+  const { contacts } = useContacts();
+  const { createConversation } = useConversations();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    createConversation(selectedContactIds);
+    closeModal();
+  }
+
+  function handleCheckBoxChange(contactId) {
+    setSelectedContactIds((prevSelectedContactIds) => {
+      if (prevSelectedContactIds.includes(contactId)) {
+        return prevSelectedContactIds.filter((prevId) => {
+          return contactId !== prevId;
+        });
+      } else {
+        return [...prevSelectedContactIds, contactId];
+      }
+    });
+  }
+  return (
+    <>
+      <Modal.Header closeButton>Create Conversation</Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          {contacts.map((contact) => (
+            <Form.Group controlId={contact.id} key={contact.id}>
+              <Form.Check
+                type="checkbox"
+                value={selectedContactIds.includes(contact.id)}
+                label={contact.name}
+                onChange={() => handleCheckBoxChange(contact.id)}
+              />
+            </Form.Group>
+          ))}
+          <Button type="submit">Create</Button>
+        </Form>
+      </Modal.Body>
+    </>
+  );
 }
